e078eee13553884744f71a6dbc7db71774bf6ca8
hotchocomel <b06207041@ntu.edu.tw>
2024-03-14 11:10:22 +0000

    channging

---
+++ dataset/data-exploration.R
@@ -0,0 +1,82 @@
+# the following exercises are a test in disguise. 
+# what do you think about the coding? 
+# can you think of any improvements to the following?
+
+rm(list=ls()) # always clean up your environment! 
+
+# install.packages("tidyverse") #uncomment to install tidyverse if you haven't already
+
+library(tidyverse)
+
+## PROBLEM 1 ##
+
+> visualizing your data is important!
+> summary statistics can be highly misleading, and simply plotting the data can reveal a lot more!
+> Lets look at the Anscombe’s Quartet data. There are four different data sets.
+> Anscombe, F. J. (1973). "Graphs in Statistical Analysis". American Statistician. 27 (1): 17–21. doi:10.1080/00031305.1973.10478966. JSTOR 2682899.
+
+anscombe_quartet = readRDS("anscombe_quartet.rds")
+
+# let's explore the dataset 
+str(anscombe_quartet)
+
+# what does the function str() do? 
+
+
+# let's check some summary statistics:
+
+anscombe_quartet %>% 
+  group_by(dataset) %>% 
+  summarise(
+    mean_x    = mean(x),
+    mean_y    = mean(y),
+    min_x     = min(x),
+    min_y     = min(y),
+    max_x     = max(x),
+    max_y     = max(y),
+    crrltn    = cor(x, y)
+  )
+
+# what do the summary statistics tell us about the different datasets? 
+
+# let's plot the data with ggplot:
+
+require(ggplot2)
+
+ ggplot(anscombe_quartet, aes(x=x,y=y)) +
+  geom_point() + 
+  geom_smooth(method = "lm",formula = "y ~ x") +
+  facet_wrap(~dataset)
+
+#save the plot
+
+ggsave("anscombe_quartet.png", width = 20, height = 20, units = "cm")
+
+# what do the plots tell us about the different datasets? 
+# describe the relationship between x and y in the different datasets. 
+
+# would linear regression be an appropriate statistical model to analyse the x-y relationship in each dataset?
+
+# what conclusions can you draw for the plots and summary statistics? 
+
+
+
+
+## PROBLEM 2 ##
+
+# load in the datasaurus dataset
+datasaurus_dozen = readRDS("datasaurus_dozen.rds")
+
+# explore the dataset 
+
+# how many rows and columns does the datasaurus_dozen file have? 
+
+# plot the dataset 
+
+# calculate the correlations and summary statistics for x and y in all datasets: 
+
+# Plot the relationships between x and y in each dataset including the line of best fit.
+
+# save the plot 
+
+# what conclusions can you draw for the plots and summary statistics? 
